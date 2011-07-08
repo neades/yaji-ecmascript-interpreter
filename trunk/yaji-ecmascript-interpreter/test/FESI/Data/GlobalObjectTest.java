c80c2c801ac24a55fc3cd4df7f7aa62e7f4a9453
kittykirstydesigns@gmail.com <kittykirstydesigns@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2011-07-08 14:48:20 +0000

    Adding test for rejected issue #75 - calling isNan(undefined); should (and does) return true.

---
+++ trunk/yaji-ecmascript-interpreter/test/FESI/Data/GlobalObjectTest.java
@@ -0,0 +1,41 @@
+package FESI.Data;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import FESI.Exceptions.EcmaScriptException;
+import FESI.Interpreter.Evaluator;
+import FESI.Util.EvaluatorAccess;
+
+
+public class GlobalObjectTest  {
+
+    private Evaluator evaluator;
+    private GlobalObject globalObject;
+    @Before
+    public void setUp() throws Exception {
+        evaluator = new Evaluator();
+        globalObject = (GlobalObject) evaluator.getGlobalObject();
+    }
+
+    @After 
+    public void tearDown() throws Exception {
+        EvaluatorAccess.setAccessor(null);
+    }
+    
+    @Test
+    public void testIsNaNOnUndefined(){
+        try {
+            ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "isNaN", new ESValue[] {ESUndefined.theUndefined});
+            assertEquals(true, result.booleanValue());
+        } catch (EcmaScriptException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
