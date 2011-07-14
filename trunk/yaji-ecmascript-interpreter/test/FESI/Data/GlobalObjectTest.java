6edfbb2abfb412e4151beb47a265d119eb0e4070
kittykirstydesigns@gmail.com <kittykirstydesigns@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2011-07-14 14:20:46 +0000

    Refactoring - better assertions and extracted arguments as constants

--- trunk/yaji-ecmascript-interpreter/test/FESI/Data/GlobalObjectTest.java
+++ trunk/yaji-ecmascript-interpreter/test/FESI/Data/GlobalObjectTest.java
@@ -1,6 +1,6 @@
 package FESI.Data;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,7 +12,10 @@ import FESI.Util.EvaluatorAccess;
 
 
 public class GlobalObjectTest  {
+    
 
+    private static final ESValue[] UNDEFINED_ARGS = new ESValue[] {ESUndefined.theUndefined};
+    private static final ESValue[] NO_ARGS = new ESValue[] {};
     private Evaluator evaluator;
     private GlobalObject globalObject;
     @Before
@@ -28,27 +31,27 @@ public class GlobalObjectTest  {
 
     @Test
     public void testIsNaNOnUndefined() throws EcmaScriptException, NoSuchMethodException{
-        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "isNaN", new ESValue[] {ESUndefined.theUndefined});
-        assertEquals(true, result.booleanValue());
+        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "isNaN", UNDEFINED_ARGS);
+        assertTrue(result.booleanValue());
     }
 
     @Test
     public void testParseFloatOnUndefined() throws EcmaScriptException, NoSuchMethodException{
-        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "parseFloat", new ESValue[] {ESUndefined.theUndefined});
-        assertEquals("NaN", result.toString());
+        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "parseFloat", UNDEFINED_ARGS);
+        assertTrue(Double.isNaN(result.doubleValue()));
     }
 
 
     @Test
     public void testParseFloatNoArgs() throws EcmaScriptException, NoSuchMethodException{
-        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "parseFloat", new ESValue[] {});
-        assertEquals("NaN", result.toString());
+        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "parseFloat", NO_ARGS);
+        assertTrue(Double.isNaN(result.doubleValue()));
     }
 
     @Test
     public void testParseIntNoArgs() throws EcmaScriptException, NoSuchMethodException{
-        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "parseInt", new ESValue[] {});
-        assertEquals("NaN", result.toString());
+        ESValue result = globalObject.doIndirectCall(evaluator, globalObject, "parseInt", NO_ARGS);
+        assertTrue(Double.isNaN(result.doubleValue()));
     }
 
 }
