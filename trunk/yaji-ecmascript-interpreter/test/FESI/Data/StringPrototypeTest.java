189dfcb4cf6c1c5f6d223aee5eef3355ecbef64f
neil.eades.at.work@gmail.com <neil.eades.at.work@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2011-08-23 09:51:03 +0000

    Some tests around charAt

--- trunk/yaji-ecmascript-interpreter/test/FESI/Data/StringPrototypeTest.java
+++ trunk/yaji-ecmascript-interpreter/test/FESI/Data/StringPrototypeTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import FESI.Exceptions.EcmaScriptException;
 import FESI.Interpreter.Evaluator;
 
 public class StringPrototypeTest {
@@ -35,5 +36,32 @@ public class StringPrototypeTest {
         ESObject originalObject = new ESString("start").toESObject(evaluator);
         ESValue result = originalObject.doIndirectCall(evaluator, originalObject, "concat", new ESValue[] { new ESString("end") });
         assertEquals(new ESString("startend"),result);
+    }
+    
+    @Test
+    public void charAtWorksWithString() throws Exception {
+        ESObject originalObject = new ESString("start").toESObject(evaluator);
+        ESValue result = originalObject.doIndirectCall(evaluator, originalObject, "charAt", new ESValue[] { ESNumber.valueOf(1) });
+        assertEquals(new ESString("t"),result);
+    }
+    
+    @Test
+    public void charAtWorksPassedString() throws Exception {
+        ESObject originalObject = new ESString("start").toESObject(evaluator);
+        ESValue result = originalObject.doIndirectCall(evaluator, originalObject, "charAt", new ESValue[] { ESNumber.valueOf(10) });
+        assertEquals(new ESString(""),result);
+    }
+    
+    @Test
+    public void charAtWorksPassedNumber() throws Exception {
+        ESObject originalObject = ESNumber.valueOf(123).toESObject(evaluator);
+        ESValue  charAtFunction = getStringPrototype().getProperty("charAt", "charAt".hashCode());
+        ESValue result = charAtFunction.callFunction(originalObject, new ESValue[] { ESNumber.valueOf(1) });
+        assertEquals(new ESString("2"),result);
+    }
+    private ESObject getStringPrototype() throws EcmaScriptException {
+        ESObject stringObject = (ESObject) evaluator.getGlobalObject().getProperty("String","String".hashCode());
+        ESObject stringPrototype = (ESObject) stringObject.getProperty(StandardProperty.PROTOTYPEstring,StandardProperty.PROTOTYPEhash);
+        return stringPrototype;
     }
 }
