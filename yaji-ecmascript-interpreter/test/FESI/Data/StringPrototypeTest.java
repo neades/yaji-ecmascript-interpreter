3e0c3163547afabda162969f15d8ead463835e46
neil.eades.at.work@gmail.com <neil.eades.at.work@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2011-09-20 15:08:19 +0000

    Issue #121 Implementing String.prototype.trim

--- yaji-ecmascript-interpreter/test/FESI/Data/StringPrototypeTest.java
+++ yaji-ecmascript-interpreter/test/FESI/Data/StringPrototypeTest.java
@@ -13,6 +13,31 @@ import FESI.Interpreter.Evaluator;
 public class StringPrototypeTest {
     private Evaluator evaluator;
 
+    private static final String NON_ASCII_WHITE_SPACE = 
+         "\u00A0" //   no-break space
+        +"\u1680" //   ogham space mark
+        +"\u180E" //   mongolian vowel separator
+        +"\u2000" //   en quad Common
+        +"\u2001" //   em quad Common
+        +"\u2002" //   en space
+        +"\u2003" //   em space
+        +"\u2004" //   three-per-em space
+        +"\u2005" //   four-per-em space
+        +"\u2006" //   six-per-em space
+        +"\u2007" //   figure space
+        +"\u2008" //   punctuation space
+        +"\u2009" //   thin space
+        +"\u200A" //   hair space 
+        +"\u2028" //   line separator
+        +"\u2029" //   paragraph separator
+        +"\u202F" //   narrow no-break space
+        +"\u205F" //   medium mathematical space 
+        +"\u3000" //   ideographic space
+        +"\u2028" //   Line separator
+        +"\u2029";//   Paragraph separator
+
+    private static final String ASCII_WHITE_SPACE = "\u0009\n\u000B\u000C\r\u0020";
+    
     @Before
     public void setUp() {
         evaluator = new Evaluator();
@@ -73,10 +98,48 @@ public class StringPrototypeTest {
         assertEquals(new ESString("x"), originalObject.getProperty("z","z".hashCode()));
     }
 
+    @Test
+    public void trimShoulRemoveLeadingSpaces() throws Exception {
+        ESObject originalObject = new ESString(" start").toESObject(evaluator);
+        ESValue trimmed = originalObject.doIndirectCall(evaluator, originalObject, "trim", ESValue.EMPTY_ARRAY);
+        assertEquals("start",trimmed.toString());
+    }
+    
+    @Test
+    public void trimShouldRemoveLeadingAndTrilingUnicodeSpaces() throws Exception {
+        ESObject originalObject = new ESString(ASCII_WHITE_SPACE +
+                 "\u0001sta rt"
+                + NON_ASCII_WHITE_SPACE
+		).toESObject(evaluator);
+        ESValue trimmed = originalObject.doIndirectCall(evaluator, originalObject, "trim", ESValue.EMPTY_ARRAY);
+        assertEquals("\u0001sta rt",trimmed.toString());
+    }
+    
+    @Test
+    public void trimShoulRemoveTrailingSpaces() throws Exception {
+        ESObject originalObject = new ESString(" start  ").toESObject(evaluator);
+        ESValue trimmed = originalObject.doIndirectCall(evaluator, originalObject, "trim", ESValue.EMPTY_ARRAY);
+        assertEquals("start",trimmed.toString());
+    }
+    
+    @Test
+    public void trimShouldTrimeEmptyString() throws Exception {
+        ESObject originalObject = new ESString("").toESObject(evaluator);
+        ESValue trimmed = originalObject.doIndirectCall(evaluator, originalObject, "trim", ESValue.EMPTY_ARRAY);
+        assertEquals("",trimmed.toString());
+    }
+    
+    @Test
+    public void trimShouldTrimeWhiteSpaceString() throws Exception {
+        ESObject originalObject = new ESString(ASCII_WHITE_SPACE+NON_ASCII_WHITE_SPACE).toESObject(evaluator);
+        ESValue trimmed = originalObject.doIndirectCall(evaluator, originalObject, "trim", ESValue.EMPTY_ARRAY);
+        assertEquals("",trimmed.toString());
+    }
     
     private ESObject getStringPrototype() throws EcmaScriptException {
         ESObject stringObject = (ESObject) evaluator.getGlobalObject().getProperty("String","String".hashCode());
         ESObject stringPrototype = (ESObject) stringObject.getProperty(StandardProperty.PROTOTYPEstring,StandardProperty.PROTOTYPEhash);
         return stringPrototype;
     }
+    
 }
