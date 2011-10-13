1455112d83b6a7db9860ba8029216de35ce73032
neil.eades.at.work@gmail.com <neil.eades.at.work@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2011-10-13 12:08:44 +0000

    Issue #138,#139.#140 & #141 - Implementing toDateString, toTimeString and locale versions

--- trunk/yaji-ecmascript-interpreter/test/FESI/Data/DatePrototypeTest.java
+++ trunk/yaji-ecmascript-interpreter/test/FESI/Data/DatePrototypeTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertSame;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import org.junit.Before;
@@ -20,14 +21,13 @@ public class DatePrototypeTest extends EvaluatorTestCase {
     @Before
     public void setUp() throws Exception {
         super.setUp();
+        evaluator.setDefaultTimeZone(TimeZone.getTimeZone("Asia/Jakarta"));
+        evaluator.setDefaultLocale(new Locale("id"));
     }
     
     @Test
     public void shouldConvertDateToIsoFormat() throws  Exception {
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        calendar.clear();
-        calendar.set(1965, 9, 30, 14, 22, 36);
-        Date date = new Date(calendar.getTimeInMillis()+123L);
+        Date date = createDate();
         DatePrototype dp = (DatePrototype) dateObject.doConstruct(ESValue.EMPTY_ARRAY);
         dp.setDate(date);
         ESValue value = dp.doIndirectCall(evaluator, dp, "toISOString", ESValue.EMPTY_ARRAY);
@@ -42,10 +42,7 @@ public class DatePrototypeTest extends EvaluatorTestCase {
     
     @Test
     public void shouldConvertDateToJSONFormat() throws  Exception {
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        calendar.clear();
-        calendar.set(1965, 9, 30, 14, 22, 36);
-        Date date = new Date(calendar.getTimeInMillis()+123L);
+        Date date = createDate();
         DatePrototype dp = (DatePrototype) dateObject.doConstruct(ESValue.EMPTY_ARRAY);
         dp.setDate(date);
         ESValue value = dp.doIndirectCall(evaluator, dp, "toJSON", ESValue.EMPTY_ARRAY);
@@ -54,10 +51,7 @@ public class DatePrototypeTest extends EvaluatorTestCase {
     
     @Test
     public void toJSONshouldCalltoISOString() throws  Exception {
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        calendar.clear();
-        calendar.set(1965, 9, 30, 14, 22, 36);
-        Date date = new Date(calendar.getTimeInMillis()+123L);
+        Date date = createDate();
         DatePrototype dp = (DatePrototype) dateObject.doConstruct(ESValue.EMPTY_ARRAY);
         dp.setDate(date);
         dp.putProperty("toISOString", TestHelpers.createFunction(evaluator, "return 'overridden';"));
@@ -67,14 +61,19 @@ public class DatePrototypeTest extends EvaluatorTestCase {
     
     @Test(expected=TypeError.class)
     public void toJSONshouldThrowTypeErrorIftoISOStringNotFunction() throws  Exception {
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        calendar.clear();
-        calendar.set(1965, 9, 30, 14, 22, 36);
-        Date date = new Date(calendar.getTimeInMillis()+123L);
+        Date date = createDate();
         DatePrototype dp = (DatePrototype) dateObject.doConstruct(ESValue.EMPTY_ARRAY);
         dp.setDate(date);
         dp.putProperty("toISOString", new ESString("return 'overridden';"));
         dp.doIndirectCall(evaluator, dp, "toJSON", ESValue.EMPTY_ARRAY);
+    }
+
+    private Date createDate() {
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.clear();
+        calendar.set(1965, 9, 30, 14, 22, 36);
+        Date date = new Date(calendar.getTimeInMillis()+123L);
+        return date;
     }
     
     @Test
@@ -89,5 +88,46 @@ public class DatePrototypeTest extends EvaluatorTestCase {
     public void constructorShouldBeSet() throws Exception {
         DatePrototype dp = (DatePrototype) dateObject.doConstruct(ESValue.EMPTY_ARRAY);
         assertSame(dateObject,dp.getProperty("constructor"));
+    }
+    
+    @Test
+    public void toDateString() throws Exception  {
+        DatePrototype dp = (DatePrototype) dateObject.doConstruct(new ESValue[] { ESNumber.valueOf(Double.NaN)});
+        dp.setDate(createDate());
+        ESValue value = dp.doIndirectCall(evaluator, dp, "toDateString", ESValue.EMPTY_ARRAY);
+        assertEquals(new ESString("Sat Oct 30 1965"),value);
+    }
+    
+    @Test
+    public void toTimeString() throws Exception  {
+        DatePrototype dp = (DatePrototype) dateObject.doConstruct(new ESValue[] { ESNumber.valueOf(Double.NaN)});
+        dp.setDate(createDate());
+        ESValue value = dp.doIndirectCall(evaluator, dp, "toTimeString", ESValue.EMPTY_ARRAY);
+        assertEquals(new ESString("21:22:36 UTC+0700"),value);
+    }
+    
+    @Test
+    public void toLocaleDateString() throws Exception  {
+        DatePrototype dp = (DatePrototype) dateObject.doConstruct(new ESValue[] { ESNumber.valueOf(Double.NaN)});
+        dp.setDate(createDate());
+        ESValue value = dp.doIndirectCall(evaluator, dp, "toLocaleDateString", ESValue.EMPTY_ARRAY);
+        assertEquals(new ESString("1965 Okt 30"),value);
+    }
+    
+    @Test
+    public void toLocaleDateStringUK() throws Exception  {
+        evaluator.setDefaultLocale(Locale.UK);
+        DatePrototype dp = (DatePrototype) dateObject.doConstruct(new ESValue[] { ESNumber.valueOf(Double.NaN)});
+        dp.setDate(createDate());
+        ESValue value = dp.doIndirectCall(evaluator, dp, "toLocaleDateString", ESValue.EMPTY_ARRAY);
+        assertEquals(new ESString("30-Oct-1965"),value);
+    }
+    
+    @Test
+    public void toLocaleTimeString() throws Exception  {
+        DatePrototype dp = (DatePrototype) dateObject.doConstruct(new ESValue[] { ESNumber.valueOf(Double.NaN)});
+        dp.setDate(createDate());
+        ESValue value = dp.doIndirectCall(evaluator, dp, "toLocaleTimeString", ESValue.EMPTY_ARRAY);
+        assertEquals(new ESString("21:22:36 WIT"),value);
     }
 }
