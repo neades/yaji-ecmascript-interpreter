83f000d7499efe23969a3f3530a6faa099279760
neil.eades.at.work@gmail.com <neil.eades.at.work@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2011-11-01 21:16:06 +0000

    Issue #165 Length of arguments no longer in standard environment for function execution.

---
+++ yaji-ecmascript-interpreter/test/FESI/Data/ESArgumentsTest.java
@@ -0,0 +1,83 @@
+package FESI.Data;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ESArgumentsTest extends EvaluatorTestCase {
+
+    private ESArguments environment;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        String[] argumentNames = new String[] { "arg1", "arg2" };
+        ESValue[] argumentValues = new ESValue[] { ESNumber.valueOf(100), new ESString("value") };
+        ESObject callee = createFunction("return false;");
+        environment = ESArguments.makeNewESArguments(evaluator, callee, argumentNames, argumentValues);
+    }
+
+    @Test
+    public void argumentsObjectShouldHaveClassArguments() throws Exception {
+        ESObject arguments = (ESObject) environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        assertEquals("Arguments",arguments.getESClassName());
+    }
+
+    @Test
+    public void argumentsObjectShouldHaveLength() throws Exception {
+        ESObject arguments = (ESObject) environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        assertEquals(ESNumber.valueOf(2),arguments.getProperty(StandardProperty.LENGTHstring));
+    }
+    
+    @Test
+    public void environmentShouldHaveProperty() throws Exception {
+        assertEquals(ESNumber.valueOf(100),environment.getProperty("arg1"));
+    }
+    
+    @Test
+    public void argumentsShouldHaveIndexedArguments() throws Exception {
+        ESObject arguments = (ESObject) environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        assertEquals(ESNumber.valueOf(100),arguments.getProperty("0"));
+    }
+    
+    @Test
+    public void argumentsShouldHaveDirectlyIndexedArguments() throws Exception {
+        ESObject arguments = (ESObject) environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        assertEquals(ESNumber.valueOf(100),arguments.getProperty(0));
+    }
+    
+    @Test
+    public void argumentsShouldAllowAccessByName() throws Exception {
+        ESObject arguments = (ESObject) environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        assertEquals(ESNumber.valueOf(100),arguments.getProperty("arg1"));
+    }
+    
+    @Test
+    public void argumentsShouldAllowArgumentsToBeOverridden() throws Exception {
+        environment.putProperty("arguments",ESNumber.valueOf(42),"arguments".hashCode());
+        ESValue arguments = environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        assertEquals(ESNumber.valueOf(42),arguments);
+    }
+    
+    @Test
+    public void getArgumentValueAfterNamedArguments() throws Exception {
+        String[] argumentNames = new String[] { "arg1" };
+        ESValue[] argumentValues = new ESValue[] { ESNumber.valueOf(100), new ESString("value") };
+        ESObject callee = createFunction("return false;");
+        environment = ESArguments.makeNewESArguments(evaluator, callee, argumentNames, argumentValues);
+
+        ESObject arguments = (ESObject) environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        assertEquals(ESNumber.valueOf(2),arguments.getProperty("length"));
+        assertEquals(new ESString("value"), arguments.getProperty(1));
+    }
+
+    @Test
+    public void argumentsShouldAllowValuesToBeDeleted() throws Exception {
+        ESObject arguments = (ESObject) environment.getPropertyInScope("arguments",null,"arguments".hashCode());
+        arguments.deleteProperty("0", "0".hashCode());
+        assertEquals(ESUndefined.theUndefined,arguments.getProperty(0));
+    }
+    
+}
