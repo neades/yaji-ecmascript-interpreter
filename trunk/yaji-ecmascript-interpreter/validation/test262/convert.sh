a20bbba7152db94bc5a84c241d38c3c4e2000a2b
neil.eades.at.work@gmail.com <neil.eades.at.work@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2012-02-19 21:56:23 +0000

    Better handling of function fixing

--- trunk/yaji-ecmascript-interpreter/validation/test262/convert.sh
+++ trunk/yaji-ecmascript-interpreter/validation/test262/convert.sh
@@ -8,7 +8,7 @@ for i in `cat /tmp/convert.$$` ; do
   echo $testName [converted/$dirName]
   mkdir -pv converted/$dirName
   echo "python tools/packaging/test262.py --command 'echo {{path}}' $testName --cat > converted/${testName}.js"
-  python tools/packaging/test262.py --command 'echo {{path}}' $testName --cat |  sed 's/\(^[[:blank:]][[:blank:]]*\)function[[:blank:]]*\([a-zA-Z0-9_$]*\)/\1var \2 = function \2/' > converted/${testName}.js
+  python tools/packaging/test262.py --command 'echo {{path}}' $testName --cat |  sed 's/\(^[[:blank:]][[:blank:]]*\)function[[:blank:]]*\([a-zA-Z0-9_$][a-zA-Z0-9_$]*\)/\1var \2 = function \2/' > converted/${testName}.js
 
 done
 
