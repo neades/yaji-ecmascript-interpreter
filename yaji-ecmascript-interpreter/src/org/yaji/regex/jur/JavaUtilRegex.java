46460cc856816035f413aa6240f1f7e45287781f
neil.eades.at.work@gmail.com <neil.eades.at.work@gmail.com@bb5254d4-1650-ec22-02e7-c98641c9763a>
2012-02-27 21:54:13 +0000

    Only applying DOTALL for multiline

--- yaji-ecmascript-interpreter/src/org/yaji/regex/jur/JavaUtilRegex.java
+++ yaji-ecmascript-interpreter/src/org/yaji/regex/jur/JavaUtilRegex.java
@@ -1,20 +1,21 @@
 package org.yaji.regex.jur;
 
 import java.util.Formatter;
+import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-import org.yaji.regex.Pattern;
 import org.yaji.regex.Regex;
 import org.yaji.regex.RegexImplementation;
+
 
 import FESI.Exceptions.EcmaScriptException;
 import FESI.Exceptions.SyntaxError;
 
 public class JavaUtilRegex implements RegexImplementation {
 
-    public Pattern compile(String regex, int flags) throws EcmaScriptException {
+    public org.yaji.regex.Pattern compile(String regex, int flags) throws EcmaScriptException {
         try {
-        return new JavaUtilRegexPattern(java.util.regex.Pattern.compile(convertRegExp(regex), convertFlags(flags)|java.util.regex.Pattern.DOTALL));
+        return new JavaUtilRegexPattern(Pattern.compile(convertRegExp(regex), convertFlags(flags)));
         } catch(PatternSyntaxException e) {
             throw new SyntaxError("PatternSyntaxException: /" + regex + "/", e);
 
@@ -22,8 +23,8 @@ public class JavaUtilRegex implements RegexImplementation {
     }
     
     private static int [][] flagsMap = {
-        { Regex.MULTILINE, java.util.regex.Pattern.MULTILINE },
-        { Regex.CASE_INSENSITIVE, java.util.regex.Pattern.CASE_INSENSITIVE }
+        { Regex.MULTILINE, Pattern.MULTILINE|Pattern.DOTALL },
+        { Regex.CASE_INSENSITIVE, Pattern.CASE_INSENSITIVE }
     };
     private int convertFlags(int flags) {
         int converted = 0;
@@ -36,16 +37,16 @@ public class JavaUtilRegex implements RegexImplementation {
     }
     
     private static abstract class State {
-        private static class DecimalEscapeState extends JavaUtilRegex.State {
+        private static class DecimalEscapeState extends State {
             private int decimal = 0;
-            private final JavaUtilRegex.State parentState;
+            private final State parentState;
             
-            public DecimalEscapeState(char c, JavaUtilRegex.State parentState) {
+            public DecimalEscapeState(char c, State parentState) {
                 this.parentState = parentState;
                 decimal = c - '0';
             }
             @Override
-            public JavaUtilRegex.State process(char c, StringBuilder sb) {
+            public State process(char c, StringBuilder sb) {
                 if (Character.isDigit(c)) {
                     decimal = 8*decimal + (c-'0');
                     return this;
@@ -56,9 +57,9 @@ public class JavaUtilRegex implements RegexImplementation {
                 return parentState.process(c,sb);
             }
         }
-        public final static JavaUtilRegex.State NORMAL = new State() {
+        public final static State NORMAL = new State() {
             @Override
-            public JavaUtilRegex.State process(char c, StringBuilder sb) {
+            public State process(char c, StringBuilder sb) {
                 if (c == '\\') {
                     return NORMAL_INESCAPE;
                 }
@@ -69,17 +70,17 @@ public class JavaUtilRegex implements RegexImplementation {
                 return this;
             }
         };
-        public final static JavaUtilRegex.State NORMAL_INESCAPE = new State() {
+        public final static State NORMAL_INESCAPE = new State() {
             @Override
-            public JavaUtilRegex.State process(char c, StringBuilder sb) {
+            public State process(char c, StringBuilder sb) {
                 sb.append('\\');
                 sb.append(c);
                 return NORMAL;
             }
         };
-        public final static JavaUtilRegex.State CLASSRANGE_INESCAPE = new State() {
+        public final static State CLASSRANGE_INESCAPE = new State() {
             @Override
-            public JavaUtilRegex.State process(char c, StringBuilder sb) {
+            public State process(char c, StringBuilder sb) {
                 if (c == 'b') {
                     sb.append("\u0008");
                 } else if (Character.isDigit(c)) {
@@ -91,9 +92,9 @@ public class JavaUtilRegex implements RegexImplementation {
                 return CLASSRANGE;
             }
         };
-        public final static JavaUtilRegex.State CLASSRANGE = new State() {
+        public final static State CLASSRANGE = new State() {
             @Override
-            public JavaUtilRegex.State process(char c, StringBuilder sb) {
+            public State process(char c, StringBuilder sb) {
                 if (c == '\\') {
                     return CLASSRANGE_INESCAPE;
                 }
@@ -105,9 +106,9 @@ public class JavaUtilRegex implements RegexImplementation {
                 return this;
             }
         };
-        public final static JavaUtilRegex.State FIRSTINCLASSRANGE = new State() {
+        public final static State FIRSTINCLASSRANGE = new State() {
             @Override
-            public JavaUtilRegex.State process(char c, StringBuilder sb) {
+            public State process(char c, StringBuilder sb) {
                 if (c ==']') {
                     sb.append("[^\\w\\W\\u0000]");
                     return NORMAL;
@@ -119,9 +120,9 @@ public class JavaUtilRegex implements RegexImplementation {
                 return CLASSRANGE.process(c, sb);
             }
         };
-        public final static JavaUtilRegex.State FIRSTININVERTEDCLASSRANGE = new State() {
+        public final static State FIRSTININVERTEDCLASSRANGE = new State() {
             @Override
-            public JavaUtilRegex.State process(char c, StringBuilder sb) {
+            public State process(char c, StringBuilder sb) {
                 if (c ==']') {
                     sb.append('.');
                     return NORMAL;
@@ -133,14 +134,14 @@ public class JavaUtilRegex implements RegexImplementation {
         };
         
         
-        public abstract JavaUtilRegex.State process(char c, StringBuilder sb);
+        public abstract State process(char c, StringBuilder sb);
     }
-    private static java.util.regex.Pattern shouldConvert = java.util.regex.Pattern.compile("((\\\\b)|(\\[(\\^)?\\])|(\\\\\\d))");
+    private static Pattern shouldConvert = Pattern.compile("((\\\\b)|(\\[(\\^)?\\])|(\\\\\\d))");
     protected static String convertRegExp(String source) {
        
         if (shouldConvert.matcher(source).find()) {
             StringBuilder sb = new StringBuilder(source.length());
-            JavaUtilRegex.State state = State.NORMAL;
+            State state = State.NORMAL;
             for( char c : source.toCharArray()) {
                state = state.process(c, sb); 
             }
